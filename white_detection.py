2efefc15753236c242871f5ee5b70a2ede732677
Momoko_Iwagami <iwagami.m0@gmail.com>
2023-11-17 15:51:27 +0000

    white pixel

--- white_detection.py
+++ white_detection.py
@@ -2,39 +2,26 @@ import pandas as pd
 import numpy as np
 import cv2
 import glob 
-import matplotlib.pylab as plt
-import time
+import os
+from tkinter import Tk
+from tkinter.filedialog import askdirectory 
 
 class DetectPixels:
-    def __init__(self, filename):
+    def __init__(self, filename=None):
       self.filename = filename
         
         
-    def read_print_image(filename):
+    def read_cvt_gray_image(self, filename):
         image = cv2.imread(filename)
         
-        #To display and show the shape of the image
-        #print(f"shape of the image: {(image.shape)}") 
-        #fig, ax = plt.subplots(figsize=(10,10))
-        #ax.imshow(image)
-
-        return image
-    
-    def to_grayscale(image):
-        # Use the cvtColor() function to grayscale the image 
-        img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+        # Check image files are readable
+        #if image is None:
+        #    print(f"Error: Unable to read the image at {filename}")
         
-        #to display the image
-        #window_name='Grayscale Conversion OpenCV'
-        #cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-        #cv2.imshow(window_name,img_gray)
-        #cv2.waitKey(5000)
-        #cv2.destroyAllWindows()
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        return gray_image
         
-        return img_gray
-
-        
-    def calculate_white_pixel(image):
+    def calculate_white_pixel(self, image):
         # Flatten the image and create a pandas Series
         pixel_dist = pd.Series(image.flatten(), name='pixel')
         # Create a DataFrame with the 'index' column
@@ -42,26 +29,42 @@ class DetectPixels:
         non_white_pixel_count = df.loc[(df['pixel'] != 255)]
         white_pixel_count = (len(df['pixel']) - len(non_white_pixel_count))
         white_pixel_percentage = white_pixel_count / len(df['pixel']) *100
-        
-        print(f"White colour pixel: {round(white_pixel_percentage,3)} %")
         return white_pixel_percentage
     
 
-    def detext_blank_page(percentage):
+    def detect_blank_page(self, percentage):
         if percentage == 100:
             return True
         else:
+            
             return False
 
+    def save_result(self, save_filename):
+        # To ask a folder
+        Tk().withdraw()
+        path = askdirectory(title = "Select a folder")
+        walker = os.walk(path)
+
+        outlet = pd.DataFrame(columns=['File Name', 'WhitePix', 'Result', 'Dir_name' ])
+        dict_list = []
+        for folder, subfoler, files in walker:
+            for file in files:
+                filepath = os.path.join(folder,file)
+                filename = os.path.basename(filepath)
+                dirname = os.path.dirname(filepath)
+                dirname = os.path.basename(dirname)
+                
+                image_gray = self.read_cvt_gray_image(filepath)
+                percentage = self.calculate_white_pixel(image_gray)
+                result = self.detect_blank_page(percentage)
+                row_dict = {'File Name': filename, 'WhitePix': percentage, "Result" : result, "Folder Name" : dirname}
+                dict_list.append(row_dict)
+        outlet = pd.DataFrame.from_dict(dict_list)    
+        #outlet.to_string(save_filename)
+        outlet.to_csv(save_filename)
 
 
-filename = "ultrasound.jpg"
-white_image = DetectPixels.read_print_image(filename)
-image_gray = DetectPixels.to_grayscale(white_image)
-percentage = DetectPixels.calculate_white_pixel(image_gray)
-DetectPixels.detext_blank_page(percentage)
+        
 
-
-
-#Free_Test_Data_1MB_PDF.jpg
-#non-text-searchable.jpg
+detect = DetectPixels()
+detect.save_result("white_pixel.csv")
